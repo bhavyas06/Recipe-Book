44a1a2f39bcb44024ff466db18efb19bd05330b0
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-11-27 08:08:05 +0530

    installing to react-router-dom to configure routes

---
+++ FrontEnd/routes.jsx
@@ -0,0 +1,24 @@
+import { Layout } from "./layout.jsx";
+// import { AboutUs } from "./components/aboutUs/index.jsx";
+// import AddRecipe from "./components/addRecipe/index.jsx"
+
+export const routes = [
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    // children: [
+    //   {
+    //     path: "/signup",
+    //     element: <SignUp></SignUp>,
+    //   },
+    //   {
+    //     path: "/aboutUs",
+    //     element: <AboutUs></AboutUs>
+    //   },
+    //   {
+    //     path: "/addRecipe",
+    //     element: <AddRecipe></AddRecipe>
+    //   }
+    // ],
+  },
+];
